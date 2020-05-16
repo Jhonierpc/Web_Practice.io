ded8e653a66a8897d76169d0bdfd8e0f0d095047
Jhonier Riascos Zapata <jhonierrz@gmail.com>
2020-05-15 20:46:16 -0500

    Agregamos proyecto de mi curriculum

--- js/main.js
+++ js/main.js
@@ -7,4 +7,22 @@ const grid = new Muuri('.grid', {
 window.addEventListener('load', () => {
     grid.refreshItems().layout();
     document.getElementById('grid').classList.add('imagenes-cargadas');
+
+    const enlaces = document.querySelectorAll('#categorias a');
+    enlaces.forEach((elemento) => {
+        elemento.addEventListener('click', (evento) => {
+            evento.preventDefault();
+            enlaces.forEach((enlace) => enlace.classList.remove('activo'));
+            evento.target.classList.add('activo');
+
+            const categoria = evento.target.innerHTML.toLowerCase();
+            categoria === 'todos' ? grid.filter('[data-categoria]') : grid.filter(`[data-categoria="${categoria}"]`);
+        })
+    });
+
+    // Barra de busqueda
+    document.querySelector('#barra-busqueda').addEventListener('input', (evento) => {
+        const busqueda = evento.target.value;
+        grid.filter((item) => item.getElement().dataset.etiquetas.includes(busqueda));
+    })
 })
